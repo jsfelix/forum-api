ba156ae1c8e63621f161bcfd6ab511bc424acf84
Jefferson Felix <jefferson@felixtech.dev>
2023-10-22 00:16:26 -0300

    fix: typo

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -11,7 +11,7 @@ describe('Create Question Use Case', () => {
     sut = new AnswerQuestionUseCase(answerRepository)
   })
 
-  it('should be able to create a question', async () => {
+  it('should be able to answer a question', async () => {
     const { answer } = await sut.execute({
       questionId: '1',
       instructorId: '2',
